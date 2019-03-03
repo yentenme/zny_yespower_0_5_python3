b56edfe9f3f218dc7f9ab76db0e03365a4199751
yutotetuota <yuto_tetuota@yahoo.co.jp>
2019-03-03 19:46:40 +0900

    setting python package

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ bell_yespower_module = Extension('bell_yespower',
                             include_dirs=['.'])
 
 setup (name = 'bell_yespower',
-       version = '1.0',
+       version = '1.0.1',
+       author_email = 'yuto_tetuota@yahoo.co.jp',
+       author = 'yutotetota',
+       url = 'https://github.com/bellcoin-electrum/bell_yespower_python3',
        description = 'Bindings for yespower-1.0 proof of work used by bellcoin',
        ext_modules = [bell_yespower_module])
