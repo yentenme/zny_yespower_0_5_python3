ffb573f2115cdd6c8759a6b70661f7c498bb09af
y-chan <34832037+y-chan@users.noreply.github.com>
2019-04-21 13:19:44 +0900

    fix bitzeny

--- yespower.c
+++ yespower.c
@@ -1,9 +1,9 @@
 #include "yespower.h"
 /* 
- * yespower for bellcoin
+ * yespower for bitzeny
  */
 int yespower_hash(const char *input, char *output)
 {
-	yespower_params_t params = {YESPOWER_1_0, 2048, 32, NULL, 0};
+	yespower_params_t params = {YESPOWER_0_5, 2048, 8, "Client Key", 10};
 	return yespower_tls(input, 80, &params, (yespower_binary_t *) output);
 }
