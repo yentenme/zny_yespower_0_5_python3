9292bd441f49a0a01f1ca4a154be45b9a22ff2aa
YenTen dot Me <support@yenten.me>
2020-06-25 17:59:58 +0000

    modified for yespower R16, adding a dummy solo miner

--- yespower.c
+++ yespower.c
@@ -4,6 +4,7 @@
  */
 int yespower_hash(const char *input, char *output)
 {
-	yespower_params_t params = {YESPOWER_0_5, 2048, 8, "Client Key", 10};
+	yespower_params_t params = {YESPOWER_1_0, 4096, 16, NULL, 0};
+	//yespower_params_t params = {YESPOWER_0_5, 2048, 8, "Client Key", 10};
 	return yespower_tls(input, 80, &params, (yespower_binary_t *) output);
 }
